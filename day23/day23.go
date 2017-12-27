d7818623b2ae8cb101c1998fb7230f6481b83486
Jurgen Kluft <jurgen.kluft@gmail.com>
2017-12-27 16:57:14 +0800

    * finished day 23

--- day23/day23.go
+++ day23/day23.go
@@ -1,65 +1,141 @@
 package day23
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	//"strconv"
-	//"strings"
 )
 
-func iterateOverLinesInTextFile(filename string, action func(string)) {
-	// Open the file.
-	f, _ := os.Open(filename)
-	defer f.Close()
+func solve() int {
+	cnt := 0
 
-	// Create a new Scanner for the file.
-	scanner := bufio.NewScanner(f)
+	var a, b, c, d, e, f, g, h int
 
-	// Loop over all lines in the file and print them.
-	for scanner.Scan() {
-		line := scanner.Text()
-		action(line)
+	a = 0
+	b = 99
+	c = b
+	d = 0
+	e = 0
+	f = 0
+	g = 0
+	h = 0
+
+	if a != 0 {
+		goto a1
 	}
-}
-
-// Solution is the hex-grid
-type Solution struct {
-}
-
-func (s *Solution) solve() int {
-	return 0
-}
-
-func (s *Solution) solve2() int {
-	return 0
-}
-
-func readLine(line string, s *Solution) {
-	return
-}
-
-func read(filename string) *Solution {
-	s := &Solution{}
-
-	reader := func(line string) {
-		readLine(line, s)
+	goto a5
+a1:
+	b *= 100
+	b -= -100000
+	c = b
+	c -= -17000
+a5:
+m23:
+	f = 1
+	d = 2
+g13:
+	e = 2
+g8:
+	g = d
+	g = g * e
+	cnt++
+	g = g - b
+	if g != 0 {
+		goto g2
 	}
-	iterateOverLinesInTextFile(filename, reader)
+	f = 0
+g2:
+	e -= -1
+	g = e
+	g = g - b
+	if g != 0 {
+		goto g8
+	}
+	d -= -1
+	g = d
+	g = g - b
+	if g != 0 {
+		goto g13
+	}
+	if f != 0 {
+		goto f2
+	}
+	h++
+f2:
+	g = b
+	g = g - c
+	if g != 0 {
+		goto g22
+	}
+	goto end
+g22:
+	b -= -17
+	goto m23
 
-	return s
+end:
+	return cnt
+}
+
+func solve2() int {
+	var a, b, c, d, f, g, h int
+
+	a = 1
+	b = 99
+	c = b
+	d = 0
+	f = 0
+	g = 0
+	h = 0
+
+	if a != 0 {
+		goto a1
+	}
+	goto a5
+a1:
+	b *= 100
+	b += 100000
+	c = b + 17000
+a5:
+m23:
+	f = 1
+	d = 2
+g13:
+	//e = b / d
+	//g8:
+	// Trying to solve
+	if (d * (b / d)) == b {
+		f = 0
+	}
+	//g2:
+	d++
+	g = d - b
+	if g != 0 {
+		goto g13
+	}
+	if f != 0 {
+		goto f2
+	}
+	h++
+f2:
+	g = b - c
+	if g != 0 {
+		goto g22
+	}
+	goto end
+g22:
+	b += 17
+	goto m23
+
+end:
+	return h
 }
 
 // Run1 is the primary solution
 func Run1(day int) {
-	var s = read(fmt.Sprintf("Day%d/input.text", day))
-	var result = s.solve()
+	var result = solve()
 	fmt.Printf("Day%d.1: . : %v \n", day, result)
 }
 
 // Run2 is the secondary solution
 func Run2(day int) {
-	var s = read(fmt.Sprintf("Day%d/input.text", day))
-	var result = s.solve2()
+	var result = solve2()
 	fmt.Printf("Day%d.2: . : %v \n", day, result)
 }
